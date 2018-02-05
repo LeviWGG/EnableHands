0e612204d67e2f3462260bfe6865a71dd59a0e53
LeviWGG <yugiop@126.com>
2018-02-05 23:13:42 +0800

    add NewsInfo

---
+++ app/src/main/java/app/main/wangliwei/enablehands/bean/NewsInfo.java
@@ -0,0 +1,474 @@
+package app.main.wangliwei.enablehands.bean;
+
+
+import java.util.List;
+
+public class NewsInfo {
+    private List<T1348647909107Bean> T1348647909107;
+
+    public List<T1348647909107Bean> getT1348647909107() {
+        return T1348647909107;
+    }
+
+    public void setT1348647909107(List<T1348647909107Bean> T1348647909107) {
+        this.T1348647909107 = T1348647909107;
+    }
+
+    public static class T1348647909107Bean {
+        /**
+         * template : normal1
+         * lmodify : 2018-02-05 22:51:22
+         * source : 新华网
+         * postid : D9TQOCD100018AOQ
+         * title : 一名中国公民在巴基斯坦遭不明身份枪手袭击身亡
+         * mtime : 2018-02-05 22:51:22
+         * hasImg : 1
+         * topic_background : http://img2.cache.netease.com/m/newsapp/reading/cover1/C1348646712614.jpg
+         * digest : 新华社伊斯兰堡2月5日电中国驻巴基斯坦卡拉奇总领事馆5日向新华社记者证实，一名中国公民当天在卡拉奇市一处公园附近遭不明身份枪手袭击，因伤势严重不幸遇难，与遇难者
+         * boardid : news2_bbs
+         * alias : Top News
+         * hasAD : 1
+         * imgsrc : http://cms-bucket.nosdn.127.net/86572abd95894081b8899c3843c0136820180205224734.png
+         * ptime : 2018-02-05 22:35:42
+         * daynum : 17567
+         * hasHead : 1
+         * order : 1
+         * votecount : 74
+         * hasCover : false
+         * docid : D9TQOCD100018AOQ
+         * tname : 头条
+         * url_3w : http://news.163.com/18/0205/22/D9TQOCD100018AOQ.html
+         * priority : 127
+         * url : http://3g.163.com/news/18/0205/22/D9TQOCD100018AOQ.html
+         * ads : [{"subtitle":"","skipType":"photoset","skipID":"00AO0001|2289691","tag":"photoset","title":"埃尔多安访罗马遭抗议 警民发生冲突","imgsrc":"http://cms-bucket.nosdn.127.net/aa8fd4029b0d4710b0f708f43bb7baf320180205214248.jpeg","url":"00AO0001|2289691"},{"subtitle":"","skipType":"photoset","skipID":"00AP0001|2289688","tag":"photoset","title":"沈阳虎园百余只东北虎 各个胖成球！","imgsrc":"http://cms-bucket.nosdn.127.net/ee32ea0e51114ba5965b010c5e43b65120180205201953.jpeg","url":"00AP0001|2289688"},{"subtitle":"","skipType":"photoset","skipID":"00AO0001|2289677","tag":"photoset","title":"三星\"太子\"获缓刑 一改愁容满脸笑意","imgsrc":"http://cms-bucket.nosdn.127.net/e6271941b5ff47a88c8208c283eb8fd120180205165333.jpeg","url":"00AO0001|2289677"},{"subtitle":"","skipType":"photoset","skipID":"00AO0001|2289674","tag":"photoset","title":"老鹰队首捧\"超级碗\" 球迷爬上红绿灯","imgsrc":"http://cms-bucket.nosdn.127.net/53822ac29b0d499ba2cdfe953d9cdb9720180205162303.jpeg","url":"00AO0001|2289674"},{"subtitle":"","skipType":"photoset","skipID":"00AO0001|2289672","tag":"photoset","title":"\"超级碗\"赛后费城发生爆炸 警民冲突","imgsrc":"http://cms-bucket.nosdn.127.net/8ca73750358e416c82ccb9ea5c33248e20180205152018.jpeg","url":"00AO0001|2289672"}]
+         * ename : androidnews
+         * replyCount : 92
+         * ltitle : 一名中国公民在巴基斯坦遭不明身份枪手袭击身亡
+         * hasIcon : false
+         * subtitle :
+         * cid : C1348646712614
+         * imgextra : [{"imgsrc":"http://cms-bucket.nosdn.127.net/74faab5b37414c3f8d3c7b1fdfa5c78f20180205201954.jpeg"},{"imgsrc":"http://cms-bucket.nosdn.127.net/871cabd45ac445aca2b17797ae98552c20180205201954.jpeg"}]
+         * skipID : 00AP0001|2289688
+         * imgsum : 5
+         * skipType : photoset
+         * photosetID : 00AP0001|2289688
+         */
+
+        private String template;
+        private String lmodify;
+        private String source;
+        private String postid;
+        private String title;
+        private String mtime;
+        private int hasImg;
+        private String topic_background;
+        private String digest;
+        private String boardid;
+        private String alias;
+        private int hasAD;
+        private String imgsrc;
+        private String ptime;
+        private String daynum;
+        private int hasHead;
+        private int order;
+        private int votecount;
+        private boolean hasCover;
+        private String docid;
+        private String tname;
+        private String url_3w;
+        private int priority;
+        private String url;
+        private String ename;
+        private int replyCount;
+        private String ltitle;
+        private boolean hasIcon;
+        private String subtitle;
+        private String cid;
+        private String skipID;
+        private int imgsum;
+        private String skipType;
+        private String photosetID;
+        private List<AdsBean> ads;
+        private List<ImgextraBean> imgextra;
+
+        public String getTemplate() {
+            return template;
+        }
+
+        public void setTemplate(String template) {
+            this.template = template;
+        }
+
+        public String getLmodify() {
+            return lmodify;
+        }
+
+        public void setLmodify(String lmodify) {
+            this.lmodify = lmodify;
+        }
+
+        public String getSource() {
+            return source;
+        }
+
+        public void setSource(String source) {
+            this.source = source;
+        }
+
+        public String getPostid() {
+            return postid;
+        }
+
+        public void setPostid(String postid) {
+            this.postid = postid;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getMtime() {
+            return mtime;
+        }
+
+        public void setMtime(String mtime) {
+            this.mtime = mtime;
+        }
+
+        public int getHasImg() {
+            return hasImg;
+        }
+
+        public void setHasImg(int hasImg) {
+            this.hasImg = hasImg;
+        }
+
+        public String getTopic_background() {
+            return topic_background;
+        }
+
+        public void setTopic_background(String topic_background) {
+            this.topic_background = topic_background;
+        }
+
+        public String getDigest() {
+            return digest;
+        }
+
+        public void setDigest(String digest) {
+            this.digest = digest;
+        }
+
+        public String getBoardid() {
+            return boardid;
+        }
+
+        public void setBoardid(String boardid) {
+            this.boardid = boardid;
+        }
+
+        public String getAlias() {
+            return alias;
+        }
+
+        public void setAlias(String alias) {
+            this.alias = alias;
+        }
+
+        public int getHasAD() {
+            return hasAD;
+        }
+
+        public void setHasAD(int hasAD) {
+            this.hasAD = hasAD;
+        }
+
+        public String getImgsrc() {
+            return imgsrc;
+        }
+
+        public void setImgsrc(String imgsrc) {
+            this.imgsrc = imgsrc;
+        }
+
+        public String getPtime() {
+            return ptime;
+        }
+
+        public void setPtime(String ptime) {
+            this.ptime = ptime;
+        }
+
+        public String getDaynum() {
+            return daynum;
+        }
+
+        public void setDaynum(String daynum) {
+            this.daynum = daynum;
+        }
+
+        public int getHasHead() {
+            return hasHead;
+        }
+
+        public void setHasHead(int hasHead) {
+            this.hasHead = hasHead;
+        }
+
+        public int getOrder() {
+            return order;
+        }
+
+        public void setOrder(int order) {
+            this.order = order;
+        }
+
+        public int getVotecount() {
+            return votecount;
+        }
+
+        public void setVotecount(int votecount) {
+            this.votecount = votecount;
+        }
+
+        public boolean isHasCover() {
+            return hasCover;
+        }
+
+        public void setHasCover(boolean hasCover) {
+            this.hasCover = hasCover;
+        }
+
+        public String getDocid() {
+            return docid;
+        }
+
+        public void setDocid(String docid) {
+            this.docid = docid;
+        }
+
+        public String getTname() {
+            return tname;
+        }
+
+        public void setTname(String tname) {
+            this.tname = tname;
+        }
+
+        public String getUrl_3w() {
+            return url_3w;
+        }
+
+        public void setUrl_3w(String url_3w) {
+            this.url_3w = url_3w;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        public void setPriority(int priority) {
+            this.priority = priority;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getEname() {
+            return ename;
+        }
+
+        public void setEname(String ename) {
+            this.ename = ename;
+        }
+
+        public int getReplyCount() {
+            return replyCount;
+        }
+
+        public void setReplyCount(int replyCount) {
+            this.replyCount = replyCount;
+        }
+
+        public String getLtitle() {
+            return ltitle;
+        }
+
+        public void setLtitle(String ltitle) {
+            this.ltitle = ltitle;
+        }
+
+        public boolean isHasIcon() {
+            return hasIcon;
+        }
+
+        public void setHasIcon(boolean hasIcon) {
+            this.hasIcon = hasIcon;
+        }
+
+        public String getSubtitle() {
+            return subtitle;
+        }
+
+        public void setSubtitle(String subtitle) {
+            this.subtitle = subtitle;
+        }
+
+        public String getCid() {
+            return cid;
+        }
+
+        public void setCid(String cid) {
+            this.cid = cid;
+        }
+
+        public String getSkipID() {
+            return skipID;
+        }
+
+        public void setSkipID(String skipID) {
+            this.skipID = skipID;
+        }
+
+        public int getImgsum() {
+            return imgsum;
+        }
+
+        public void setImgsum(int imgsum) {
+            this.imgsum = imgsum;
+        }
+
+        public String getSkipType() {
+            return skipType;
+        }
+
+        public void setSkipType(String skipType) {
+            this.skipType = skipType;
+        }
+
+        public String getPhotosetID() {
+            return photosetID;
+        }
+
+        public void setPhotosetID(String photosetID) {
+            this.photosetID = photosetID;
+        }
+
+        public List<AdsBean> getAds() {
+            return ads;
+        }
+
+        public void setAds(List<AdsBean> ads) {
+            this.ads = ads;
+        }
+
+        public List<ImgextraBean> getImgextra() {
+            return imgextra;
+        }
+
+        public void setImgextra(List<ImgextraBean> imgextra) {
+            this.imgextra = imgextra;
+        }
+
+        public static class AdsBean {
+            /**
+             * subtitle :
+             * skipType : photoset
+             * skipID : 00AO0001|2289691
+             * tag : photoset
+             * title : 埃尔多安访罗马遭抗议 警民发生冲突
+             * imgsrc : http://cms-bucket.nosdn.127.net/aa8fd4029b0d4710b0f708f43bb7baf320180205214248.jpeg
+             * url : 00AO0001|2289691
+             */
+
+            private String subtitle;
+            private String skipType;
+            private String skipID;
+            private String tag;
+            private String title;
+            private String imgsrc;
+            private String url;
+
+            public String getSubtitle() {
+                return subtitle;
+            }
+
+            public void setSubtitle(String subtitle) {
+                this.subtitle = subtitle;
+            }
+
+            public String getSkipType() {
+                return skipType;
+            }
+
+            public void setSkipType(String skipType) {
+                this.skipType = skipType;
+            }
+
+            public String getSkipID() {
+                return skipID;
+            }
+
+            public void setSkipID(String skipID) {
+                this.skipID = skipID;
+            }
+
+            public String getTag() {
+                return tag;
+            }
+
+            public void setTag(String tag) {
+                this.tag = tag;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getImgsrc() {
+                return imgsrc;
+            }
+
+            public void setImgsrc(String imgsrc) {
+                this.imgsrc = imgsrc;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+        }
+
+        public static class ImgextraBean {
+            /**
+             * imgsrc : http://cms-bucket.nosdn.127.net/74faab5b37414c3f8d3c7b1fdfa5c78f20180205201954.jpeg
+             */
+
+            private String imgsrc;
+
+            public String getImgsrc() {
+                return imgsrc;
+            }
+
+            public void setImgsrc(String imgsrc) {
+                this.imgsrc = imgsrc;
+            }
+        }
+    }
+}
