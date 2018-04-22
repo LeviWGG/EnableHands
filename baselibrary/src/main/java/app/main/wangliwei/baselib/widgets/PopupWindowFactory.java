d0bbcadf3d23e7679926b4db22b633e68c75de7e
LeviWGG <yugiop@126.com>
2018-04-22 22:46:09 +0800

    fix PopUpWindow can't show on oppo

--- baselibrary/src/main/java/app/main/wangliwei/baselib/widgets/PopupWindowFactory.java
+++ baselibrary/src/main/java/app/main/wangliwei/baselib/widgets/PopupWindowFactory.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 
@@ -78,6 +79,7 @@ public class PopupWindowFactory extends PopupWindow {
         recyclerView.setAdapter(popUpWindowAdapter);
         popUpWindowAdapter.setNewData(listItem);
         setWidth(view.getWidth());
+        setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
         setFocusable(true);
         setContentView(windowView);
         setBackgroundDrawable(new ColorDrawable(0));
